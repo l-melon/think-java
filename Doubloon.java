217f4218b31f9d91939396dee4d3bd7c5bdae769
adato <baolong1027@icloud.com>
2021-12-23 14:43:44 +0900

    add comment

--- Doubloon.java
+++ Doubloon.java
@@ -1,14 +1,14 @@
 public class Doubloon {
     
     public static boolean isDoubloon(String s) {
-
+        // count the number of times each letter appears
         int[] counts = new int[26];
         String lower = s.toLowerCase();
         for (char letter: lower.toCharArray()) {
             int index = letter - 'a';
             counts[index]++; 
         }
-
+        // determine whether the given word is doubloon
         for (int count: counts) {
             if (count != 0 && count != 2) {
                 return false;
