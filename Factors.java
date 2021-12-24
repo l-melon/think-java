4ae7efa512447da99787e72ea6ef40bb2a33502a
adato <baolong1027@icloud.com>
2021-12-24 13:52:27 +0900

    add method isPrime and arePrimeFactors

--- Factors.java
+++ Factors.java
@@ -8,9 +8,35 @@ public class Factors {
        return true;
    }
    
+   public static boolean isPrime(int n) {
+       if (n == 0 || n == 1) {
+           return false;
+       } else {
+           for (int i = 2; i < n; i++) {
+               if (n % i == 0) {
+                   return false;
+               }
+           }
+       }
+       return true; 
+   }
+
+
+   public static boolean arePrimeFactors(int n, int[] iArray) {
+       for (int i : iArray) {
+           if (!(isPrime(i) && areFactors(n, iArray))) {
+               return false;
+           }
+       }
+       return true;
+   }
    public static void main(String args[]) {
        int[] arrayTest = {2, 4, 6, 8};
+       int[] arrayTest2 = {2, 3};
        System.out.println(areFactors(48, arrayTest));
        System.out.println(areFactors(3, arrayTest));
+       System.out.println(isPrime(30));
+       System.out.println(arePrimeFactors(60, arrayTest2));
+
    }
 }
